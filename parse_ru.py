2db736ade2a40b094973b99c37953085aa1180c7
lixel_v <simeongfremenko@gmail.com>
2023-10-16 20:50:42 +0300

    main

---
+++ parse_ru.py
@@ -0,0 +1,26 @@
+from ast import parse
+import requests
+from bs4 import BeautifulSoup as BS
+from aiogram import types, Bot, Dispatcher, executor
+
+token = '6442961458:AAHSzpstZC7BgImelYPbuRSk3vQeJ_uzwG8'
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+def get_ru(word):
+    try:
+        r = requests.get(f'https://fonetika.su/?word={word}')
+        soup = BS(r.content, 'html.parser')
+        return soup.find('div', class_='tabs_block').find('p').get_text()
+    except Exception as e:
+        return e
+    
+@dp.message_handler(commands=['start'])
+async def hello(message: types.Message):
+    await message.answer(f'Привет `{message.from_user.id}`', parse_mode='MarkdownV2')
+    
+@dp.message_handler()
+async def get_word(message: types.Message):
+    await message.answer(f'`{get_ru(message.text)}`', parse_mode='MarkdownV2')
+    
